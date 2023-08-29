f3b17311cf82c73dbfce9712d47c0b9c9c34bfe8
Jorge Mederos <jmederosalvarado@gmail.com>
2023-08-29 11:54:07 +0200

    key bindings in wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -54,6 +54,47 @@ config.window_padding = {
 
 -- }}}
 
+-- keybindings {{{
+
+local act = wezterm.action
+
+config.disable_default_key_bindings = true
+config.leader = { key = "a", mods = "CTRL" }
+config.keys = {
+    { key = "a", mods = "LEADER|CTRL", action = act.SendKey({key = "a", mods = "CTRL"})},
+
+    -- misc
+    { key = "c", mods = "CMD", action = act.CopyTo("Clipboard") },
+    { key = "v", mods = "CMD", action = act.PasteFrom("Clipboard") },
+
+    -- tab creation
+    { key= "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+    { key= "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+
+    -- pane creation
+    { key= "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key= "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+    -- tab navigation
+    { key = "1", mods = "CMD", action = act.ActivateTab(1) },
+    { key = "2", mods = "CMD", action = act.ActivateTab(2) },
+    { key = "3", mods = "CMD", action = act.ActivateTab(3) },
+    { key = "4", mods = "CMD", action = act.ActivateTab(4) },
+    { key = "5", mods = "CMD", action = act.ActivateTab(5) },
+    { key = "6", mods = "CMD", action = act.ActivateTab(6) },
+    { key = "7", mods = "CMD", action = act.ActivateTab(7) },
+    { key = "8", mods = "CMD", action = act.ActivateTab(8) },
+    { key = "9", mods = "CMD", action = act.ActivateTab(9) },
+
+    -- pane navigation
+    { key = "h", mods = "CMD", action = act.ActivatePaneDirection("Left")},
+    { key = "j", mods = "CMD", action = act.ActivatePaneDirection("Down")},
+    { key = "k", mods = "CMD", action = act.ActivatePaneDirection("Up")},
+    { key = "l", mods = "CMD", action = act.ActivatePaneDirection("Right")},
+}
+
+-- }}}
+
 -- hyperlinks {{{
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
